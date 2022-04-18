f219a7fe2e7cf0dc15cdd1358a069b72e7091fe6
Misha Tkachenko <misha.tkachenko@telink-semi.com>
2022-04-18 22:15:22 +0300

    Updated power.c module and pm_suspend sample

--- samples/boards/telink/tlsr9518adk80d/pm_suspend/src/main.c
+++ samples/boards/telink/tlsr9518adk80d/pm_suspend/src/main.c
@@ -6,13 +6,6 @@
 
 #include <stdio.h>
 #include <zephyr.h>
-#include <device.h>
-#include <init.h>
-#include <pm/pm.h>
-#include <pm/device.h>
-#include <pm/policy.h>
-#include <soc.h>
-
 
 #define SLEEP_S 2U
 
@@ -24,6 +17,7 @@ void entry_point(int unused1, int unused2, int unused3)
     while (1) {
 		printk("System time: %lli ms \n", k_uptime_get());
 		printk("Sleeping %u s\n", SLEEP_S);
+
  		k_sleep(K_SECONDS(SLEEP_S));
 		printk("WakeUp system time: %lli ms\n", k_uptime_get());
     }
--- soc/riscv/riscv-privilege/telink_b91/power.c
+++ soc/riscv/riscv-privilege/telink_b91/power.c
@@ -4,13 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <zephyr.h>
-#include <pm/pm.h>
+
 #include <soc.h>
 #include <init.h>
-#include <sys/printk.h>
 #include <pm.h>
-#include <ext_driver/ext_pm.h>
+#include <arch/riscv/arch.h>
+
 #include <stimer.h>
+#include <pm/pm.h>
+#include <ext_driver/ext_pm.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
@@ -18,6 +20,9 @@ LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 #define CYC_PER_TICK ((uint32_t)((uint64_t) (sys_clock_hw_cycles_per_sec()			 \
 					     >> CONFIG_RISCV_MACHINE_TIMER_SYSTEM_CLOCK_DIVIDER) \
 				 / (uint64_t)CONFIG_SYS_CLOCK_TICKS_PER_SEC))
+
+#define SYS_TICK_TO_OS_TICK( tick )		( ( (uint64_t) tick / (SYSTEM_TIMER_TICK_1S/CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC)))
+
 
 /* Invoke Low Power/System Off specific Tasks */
 static uint64_t get_hart_mtimecmp(void)
@@ -48,25 +53,42 @@ static void set_mtime(uint64_t time)
     irq_enable(RISCV_MACHINE_TIMER_IRQ);
 }
 
+#define SYS_SLEEP_TIME (get_mtime_compare() - get_mtime()) / CYC_PER_TICK
+
 __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	ARG_UNUSED(substate_id);
 
-    uint32_t sleep_time = (get_mtime_compare() - get_mtime()) / CYC_PER_TICK;
-    uint64_t new_mtime = get_mtime_compare();
+    uint64_t wakeup_time = get_mtime_compare();
+	uint32_t stimer_sleep_entrance_tick = stimer_get_tick();
 
 	switch (state) {
-	case PM_STATE_SUSPEND_TO_IDLE:        
-        if (sleep_time > 0)
+	case PM_STATE_SUSPEND_TO_IDLE: 
+		// this limit is refferring to z_get_next_timeout_expiry() function, which is limiting the max timeout (K_FOREVER (-1)) to INT_MAX
+		if (wakeup_time >= INT_MAX)
+		{
+			//currently the PIN Wakeup is not implemented
+			//The k_cpu_idle will be used for endless loop
+			//The folllowing function must be enabled after PIN WAKEUP is implemented
+			//cpu_sleep_wakeup_32k_rc(SUSPEND_MODE, PM_WAKEUP_PAD, 0);
+			k_cpu_idle();
+		}
+		else 
+		if (SYS_SLEEP_TIME > 0 && wakeup_time < INT_MAX)
         {
-            cpu_sleep_wakeup_32k_rc(SUSPEND_MODE, PM_WAKEUP_TIMER | PM_WAKEUP_PAD, stimer_get_tick() + sleep_time*SYSTEM_TIMER_TICK_1MS);  //SUSPEND
-            set_mtime(new_mtime);
+            cpu_sleep_wakeup_32k_rc(SUSPEND_MODE, PM_WAKEUP_TIMER | PM_WAKEUP_PAD, stimer_get_tick() + SYS_SLEEP_TIME*SYSTEM_TIMER_TICK_1MS);  //SUSPEND
+            set_mtime(get_mtime() + SYS_TICK_TO_OS_TICK((stimer_get_tick() - stimer_sleep_entrance_tick)));
+			arch_irq_unlock(MSTATUS_IEN);
         }
+		else
+		{
+			LOG_DBG("Sleep Time = 0 or less\n");
+		}
 		break;
 	case PM_STATE_SOFT_OFF:
-		if (sleep_time > 0)
+		if (SYS_SLEEP_TIME > 0)
         {
-        	cpu_sleep_wakeup_32k_rc(DEEPSLEEP_MODE_RET_SRAM_LOW64K, PM_WAKEUP_TIMER | PM_WAKEUP_PAD, stimer_get_tick() + sleep_time*SYSTEM_TIMER_TICK_1MS);  //Deep Sleep
+        	cpu_sleep_wakeup_32k_rc(DEEPSLEEP_MODE, PM_WAKEUP_TIMER | PM_WAKEUP_PAD, stimer_get_tick() + SYS_SLEEP_TIME*SYSTEM_TIMER_TICK_1MS);  //Deep Sleep
 		}
 		break;
 	default:
@@ -82,12 +104,10 @@ __weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
    // printk("PMState Exit\n");
 	switch (state) {
 	case PM_STATE_SUSPEND_TO_IDLE:
-
+		/* Nothing to do. */
 		break;
 	case PM_STATE_SOFT_OFF:
 		/* Nothing to do. */
-
-
 		break;
 	default:
 		LOG_DBG("Unsupported power substate-id %u", state);
@@ -99,4 +119,5 @@ __weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 	 */
 	irq_unlock(0);
 }
+
 
