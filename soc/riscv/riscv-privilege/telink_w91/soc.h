e47a88168c9100ddd8fd0f02549a41b12b5ac8d3
Alex Tsitsiura <s07641069@gmail.com>
2023-11-21 14:35:11 +0200

    boards: riscv: telink: Add tlsr9118bdk40d board soc: riscv: telink_b9x: Add w91 SoC
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>

---
+++ soc/riscv/riscv-privilege/telink_w91/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Telink Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef RISCV_TELINK_W91_SOC_H
+#define RISCV_TELINK_W91_SOC_H
+
+#include <soc_common.h>
+
+#endif /* RISCV_TELINK_W91_SOC_H */
