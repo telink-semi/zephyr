cc73017342643b277be628182dd0c21b87a7cdb4
Alex Tsitsiura <s07641069@gmail.com>
2023-11-24 12:55:04 +0200

    boards: riscv: telink: Add w91 blinky sample soc: riscv: telink_b9x: Adopt start.S file
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -5,7 +5,8 @@
  */
 
 #include <zephyr/kernel.h>
-// #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio.h>
+#include "reg_include/register.h"
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -19,25 +20,66 @@
  */
 // static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+/***************************************************************************************
+ * GPIO test functionality
+ ***************************************************************************************/
+#ifndef readl
+#define readl(a) \
+	({uint32_t __v = *((volatile uint32_t *)(a)); __v; })
+#endif /* readl */
+
+#ifndef writel
+#define writel(v, a) \
+	({uint32_t __v = v; *((volatile uint32_t *)(a)) = __v; })
+#endif /* writel */
+
+static void update_reg(uint32_t addr, uint32_t value, bool set)
+{
+	uint32_t reg = readl(addr);
+	if (set) {
+		reg |= value;
+	} else {
+		reg &= ~value;
+	}
+	writel(reg, addr);
+}
+
+static void enable_gpio_out(uint8_t gpio)
+{
+	uint8_t bank  = gpio / 8;
+	uint32_t addr  = IOMUX_BASE_ADDR + bank * 4;
+	uint8_t shift = (gpio - bank * 8) * 4;
+
+	update_reg(addr, 0xf << shift, false);
+	update_reg(addr, 0x8 << shift, true);
+
+	update_reg(GPIO_BASE_ADDR + 0x0c, 1 << gpio, false);
+	update_reg(GPIO_BASE_ADDR + 0x10, 1 << gpio, true);
+}
+
+static void control_gpio_out(uint8_t gpio, bool state)
+{
+	update_reg(GPIO_BASE_ADDR + 0x14, 1 << gpio, state);
+}
+
 int main(void)
 {
-	// int ret;
+// *((volatile unsigned int *)(0xF0700028)) = *((volatile unsigned int *)(0xF0700028)) & ~(0xf << 16);
+// *((volatile unsigned int *)(0xF0700028)) = *((volatile unsigned int *)(0xF0700028)) | (0x8 << 16);
+// *((volatile unsigned int *)(0xf0700010)) = *((volatile unsigned int *)(0xf0700010)) | (1 << 20);
+// *((volatile unsigned int *)(0xf0700014)) = *((volatile unsigned int *)(0xf0700014)) | (1 << 20);
 
-	// if (!gpio_is_ready_dt(&led)) {
-	// 	return 0;
-	// }
-
-	// ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	// if (ret < 0) {
-	// 	return 0;
-	// }
-
-	// while (1) {
-	// 	ret = gpio_pin_toggle_dt(&led);
-	// 	if (ret < 0) {
-	// 		return 0;
-	// 	}
-	// 	k_msleep(SLEEP_TIME_MS);
-	// }
+	/* blink red LED */
+	enable_gpio_out(20);
+	for (;;) {
+		control_gpio_out(20, true);
+		for (size_t i = 0; i < 1000000; i++) {
+			__asm__ volatile("nop");
+		}
+		control_gpio_out(20, false);
+		for (size_t i = 0; i < 1000000; i++) {
+			__asm__ volatile("nop");
+		}
+	}
 	return 0;
 }
