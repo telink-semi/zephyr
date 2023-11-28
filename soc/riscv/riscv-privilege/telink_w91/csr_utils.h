89a6bd94f7bfa72cff122e9dcb77dac6365ca5f6
Alex Tsitsiura <s07641069@gmail.com>
2023-11-28 16:09:08 +0200

    soc: riscv: telink_b9x: Fixed FreeRTOS run on N22 core
    - Remove SRAM usage due to conflict with FreeRTOS
    - Add print of running core
    - Update start.S to latest version from Simple project
    
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>

--- soc/riscv/riscv-privilege/telink_w91/csr_utils.h
+++ soc/riscv/riscv-privilege/telink_w91/csr_utils.h
@@ -7,7 +7,10 @@
 #define NDS_MXSTATUS                 0x7c4
 #define NDS_MCACHE_CTL               0x7ca
 #define NDS_MMISC_CTL                0x7d0
-#define NDS_MHARTID                  0xf14
+#define NDS_MVENDORID                0xf11            /* vendor ID: 0000031e */
+#define NDS_MARCHID                  0xf12            /* product ID: 80000025 - D25, 80000022 - N22 */
+#define NDS_MIMPID                   0xf13            /* product version: [31:8] - major, [7:4] - minor, [3:0] - extension */
+#define NDS_MHARTID                  0xf14            /* core ID: 0 - D25, 1 - N22 */
 
 #ifndef __ASSEMBLER__
 /***************************************************************************************
