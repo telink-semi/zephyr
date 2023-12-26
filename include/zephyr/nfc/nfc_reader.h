da266aa128ed253845e82f0e918109441b62fbf8
Alex Tsitsiura <s07641069@gmail.com>
2023-12-26 14:37:37 +0200

    drivers:nfc: Add NFC driver
    Copy not released (https://github.com/zephyrproject-rtos/zephyr/pull/64142) NFC driver from https://github.com/Sendrato/zephyr/tree/v3.5-branch-nfc (Apache-2.0), which contains NRF modules (ndef, t2t, t4t, tnep) from https://github.com/nrfconnect/sdk-nrf (LicenseRef-Nordic-5-Clause).
    
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>

---
+++ include/zephyr/nfc/nfc_reader.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Sendrato
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file NFC-read subsystem
+ */
+
+#ifndef __NFC_READER_H
+#define __NFC_READER_H
+
+//todo
+
+#endif /* __NFC_READER_H */
+
