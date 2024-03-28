7e0d44523a94e26e73672c5029d8ccf529c5e66b
Andy Liu <jie.liu@telink-semi.com>
2024-03-28 16:19:04 +0200

    telink_b95: enable gpio driver
    - modify dts files
    - update gpio_b9x and pinctrl_b9x
    - restore the original code in blinky/main.c
    
    Signed-off-by: Andy Liu <jie.liu@telink-semi.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -5,60 +5,39 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 
-static void led_init(void)
-{
-	__asm__("lui     t0, 0x80140\n\t"
-		"li      t2, 0xef\n\t"
-		"li      t1, 0x10\n\t"
-		"addi    t0, t0, 0x600\n\t"
-		"addi    t0, t0, 0x600\n\t"
-		"sb      t2, 0x12(t0)\n\t" /* [0x80140c12] PB OEN = 0xef */
-		"sb      t1, 0x1c(t0)"     /* [0x80140c1c] PB Output Set = 0x10 */
-	);
-}
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
 
-static void led_toggle(void)
-{
-	__asm__("lui     t0, 0x80140\n\t"
-		"li      t1, 0x10\n\t"
-		"addi    t0, t0, 0x600\n\t"
-		"addi    t0, t0, 0x600\n\t"
-		"sb      t1, 0x1e(t0)" /* [0x80140c1e]PB Output Toggle = 0x10 */
-	);
-}
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
 
-#define TEST_TIMER_EN 1
-
-#if TEST_TIMER_EN
-struct k_timer timer;
-
-volatile int timer_counter;
-
-static void timer_expiry_cb(struct k_timer *timer)
-{
-	timer_counter++;
-
-	led_toggle();
-}
-#endif
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-	led_init();
+	int ret;
 
-#if TEST_TIMER_EN
-	k_timer_init(&timer, timer_expiry_cb, NULL);
-	k_timer_start(&timer, K_SECONDS(1), K_SECONDS(1));
-#endif
-
-	for (;;) {
-		k_msleep(1000);
-
-#if !TEST_TIMER_EN
-		led_toggle();
-#endif
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
 	return 0;
 }
