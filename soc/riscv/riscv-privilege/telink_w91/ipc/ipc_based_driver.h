e2c9adfe0b46f40e203f49a9b49af5806623032c
Borys Nykytiuk <borys.nykytiuk@telink-semi.com>
2024-08-21 16:05:35 +0300

    zephyr: drivers: watchdog: telink_w91: Add watchdog driver
    Add IPC API for the FreeRTOS watchdog
    
    Signed-off-by: Borys Nykytiuk <borys.nykytiuk@telink-semi.com>

--- soc/riscv/riscv-privilege/telink_w91/ipc/ipc_based_driver.h
+++ soc/riscv/riscv-privilege/telink_w91/ipc/ipc_based_driver.h
@@ -26,6 +26,7 @@ enum ipc_dispatcher_id {
 	IPC_DISPATCHER_BLOCKING                 = 0xa00,
 	IPC_DISPATCHER_HEARTBEAT                = 0xb00,
 	IPC_DISPATCHER_CRYPTO_ECC               = 0xc00,
+	IPC_DISPATCHER_WATCHDOG                 = 0xd00,
 } __attribute__((__packed__));
 
 typedef void (*ipc_based_driver_unpack_t)(void *result, const uint8_t *data, size_t len);
