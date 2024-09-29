3ed7686a9378de6be1368c912f9a42f998bbfb18
Alex Tsitsiura <s07641069@gmail.com>
2024-09-29 08:26:35 +0000

    soc: riscv: telink_w91: Matter dual core OTA adoptation
    - change N22 Image offset & size for Matter OTA
    - add check of n22_partition overlaps
    - download optionally Matter MCUboot files
    - use 4m flash as default
    - add check start core with switch
    - use offset and size from DTS for sctool
    - set correct reset vector & more optimizations
    
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>

--- soc/riscv/riscv-privilege/telink_w91/soc.c
+++ soc/riscv/riscv-privilege/telink_w91/soc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Telink Semiconductor
+ * Copyright (c) 2024 Telink Semiconductor
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -14,10 +14,6 @@
 #if (DT_PROP(DT_PATH(cpus, cpu_0), clock_frequency) != CLK_160MHZ)
 	#error "Unsupported clock-frequency. Supported values: 160 MHz"
 #endif
-
-#define SYS_BASE                          0xf1700000
-#define SYS_CORE_RESET_CTRL               0x218
-#define __write_reg32(addr, value)        (*(volatile uint32_t *)(addr) = (uint32_t)(value))
 
 /**
  * @brief Perform basic initialization at boot.
@@ -38,7 +34,8 @@ void sys_arch_reboot(int type)
 {
 	ARG_UNUSED(type);
 
-	__write_reg32(SYS_BASE + SYS_CORE_RESET_CTRL, 1 << (csr_read(mhartid) * 4));
+	sys_write32(D25_START_ADDR, D25_RESET_VECTOR);
+	sys_write32(D25_MASK, HART_RESET_CTRL);
 }
 
 SYS_INIT(soc_w91_init, PRE_KERNEL_1, 0);
