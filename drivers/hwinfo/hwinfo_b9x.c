27b138c151e16e5f0c8811b4b5e2468dc443cdb8
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2024-10-15 11:38:57 +0300

    Add HW info driver.

--- drivers/hwinfo/hwinfo_b9x.c
+++ drivers/hwinfo/hwinfo_b9x.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Telink Semiconductor
+ * Copyright (c) 2021-2023 Telink Semiconductor
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -14,9 +14,7 @@ ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 	uint8_t uid[16];
 
 	flash_mid = flash_read_mid();
-
 	flash_read_mid_uid_with_check((unsigned int *)&flash_mid, uid);
-
 	if (length > sizeof(uid)) {
 		length = sizeof(uid);
 	}
